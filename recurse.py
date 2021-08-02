baad5907b64c7891f2cab1d598fd9970b2ac921a
Datronizer <tqc1234@gmail.com>
2021-08-02 00:06:30 -0500

    Update 0.3
    Big system update.
    
    Main changes:
    - Rewrote switchers to actually do their jobs instead of using a ton of  if-else-if cases.
    - Rewrote print statements to return instead.
    - Combined 2 if statements from widebet() to a single one for readability
    
    Additions:
    - Added debug menu to show roll numbers (might add more into menu)

--- recurse.py
+++ recurse.py
@@ -2,8 +2,27 @@ import random
 import os
 
 def roll():
+    global verbose
     x = random.randrange(0,999)
+
+    # for bug testing roll chances
+    if verbose == True:
+        print("Current roll value: {}".format(x))
+
     return x
+
+def debug():
+    # toggles verbose on or off
+    global verbose
+    verbose = not verbose
+    if verbose == True:
+        print("############\n# Debug on #\n############")
+    else:
+        print("#############\n# Debug off #\n#############")
+
+def moneyAvailable():
+    global wallet
+    return "Cash available: {}".format(wallet)
 
 def widebet(a, b):  # a is money, b is outcome betted on
     global wallet
@@ -18,22 +37,14 @@ def widebet(a, b):  # a is money, b is outcome betted on
         # consider doing a class for this response
         wallet -= a
         
-        if x <= 499 and b == "lower":
+        if (x <= 499 and b == "lower") or (x >= 500 and b == "higher"):
             wallet += a*2
             print("Congrats! You won this round. Here's some dough.")
-            print("You received {} dollars.".format(a*2))
-            print("You currently have {} in your wallet.".format(wallet))
-            
-        elif x >= 500 and b == "higher":
-            wallet += a*2
-            print("Congrats! You won this round. Here's some dough.")
-            print("You received {} dollars".format(a*2))
-            print("You currently have {} in your wallet.".format(wallet))
+            print("You received {} dollars.".format(a*2))           
             
         else:
             print("Damn son, tough luck huh?")
             print("You lost {} dollars.".format(a))
-            print("You currently have {} in your wallet.".format(wallet))
 
 def ask():
     betoutcome = str(input("Whatcha bettin' on: "))
@@ -41,93 +52,74 @@ def ask():
     
     widebet(betmoney, betoutcome)
 
+def workRelay():
+    global workaddbuff, workmultbuff, wallet
+    work(workaddbuff, workmultbuff)
+    return "You earned " + str(wallet)
+
 def work(a, b):
-    global addbuff, multbuff
     global wallet
-    x = random.randrange(0,100)
+    #x = random.randrange(0,100)
     # Write some multiplier or additive function for profit
 
-    if x > 0 and x <= 40:
-        # Write something about line of work and how much you're getting paid
-        # Make sure to insult the player for working just to gamble :)
-        print("You tap danced with a rat for 10 dollars.")
-        wallet += 10
-        print("You currently have {} in your wallet.\n".format(wallet))
-        
-    elif x > 40 and x <= 80:
-        print("You won rock paper scissors against an amputee for 15 dollars.")
-        wallet += 15
-        print("You currently have {} in your wallet.\n".format(wallet))
-
-    elif x > 80 and x <= 90:
-        print("You licked Warren Buffett's shoe for 20 dollars.")
-        wallet += 20
-        print("You currently have {} in your wallet.\n".format(wallet))
-
-    else:
-        print("You trolled the IMF for 25 dollars.")
-        wallet += 25
-        print("You currently have {} in your wallet.\n".format(wallet))
+    # Rewrite work() to pull from dictionary with random amounts of money
+    wallet += (10 + a)*b
+    return wallet
     
 def core():
-    global playing, wallet, workaddbuff, workmultbuff
+    global playing, wallet, workaddbuff, workmultbuff, verbose
     switchers = {
-        1: "Gamble",
-        2: "Work",
-        3: "Buy",
-        4: "Craft",
-        5: "Check Wallet",
-        6: "Save",
-        7: "Load",
-        8: "Exit"
+        1: ask,
+        2: workRelay,
+        3: buy,
+        4: craft,
+        5: moneyAvailable,
+        6: save,
+        7: load,
+        8: exit,
+        
+        #secret codes
+        1974: debug
     }
 
     print("\n=========================")
     print("How can I help you today?")
     numplace = int(input("  1: Gamble,\n  2: Work,\n  3: Buy,\n  4: Craft,\n  5: Check Wallet,\n  6: Save,\n  7: Load,\n  8: Exit\n"))
 
-    place = switchers.get(numplace)
-
-    if place == "Gamble":
-        ask()
-    elif place == "Work":
-        work(workaddbuff, workmultbuff)
-        pass
-    elif place == "Buy":
-        print("Coming soon!")
-        pass
-    elif place == "Craft":
-        print("Coming soon!")
-        pass
-    elif place == "Check Wallet":
-        print("\nYou currently have {} in your wallet.".format(wallet))
-        pass
-    elif place == "Save":
-        f = open("save.txt", "w")
-        # Save format will be a list of numbers. In order:
-        # [wallet, workaddbuff, workmultbuff] (betaddbuff and betmultbuff will be added later)
-        f.write("{}\n{}\n{}".format(wallet,workaddbuff,workmultbuff))
-        f.close()
-    elif place == "Load":
-        # Load the saved file and strip the \n from the variables
-        f = open("save.txt", "r")
-        f = f.readlines()
-        f = [x.strip() for x in f]
-        wallet, workaddbuff, workmultbuff = f[0], f[1], f[2]
-    elif place == "Exit":
-        print("\nSee ya next time, chief!")
-        playing = False
-    else:
-        print("\nSorry, I didn't catch that. Come again?")
-        pass
+    place = switchers.get(numplace, lambda: "\nSorry, I didn't catch that. Come again?")
+    print(place())
 
 def buy():
-    pass
-    
+    return "Coming soon!"
 
+def craft():
+    return "Coming soon!"
+
+def load():
+    # Load the saved file and strip the \n from the variables
+    global wallet, workaddbuff, workmultbuff
+    f = open("save.txt", "r")
+    f = f.readlines()
+    f = [x.strip() for x in f]
+    wallet, workaddbuff, workmultbuff = f[0], f[1], f[2]
+    loadSuccessful = "Save loaded.\n\n" + moneyAvailable() + "\nBuffs values: {}, {}".format(workaddbuff, workmultbuff)
+    return loadSuccessful
+
+def save():
+    global wallet, workaddbuff, workmultbuff
+    f = open("save.txt", "w")
+    # Save format will be a list of numbers. In order:
+    # [wallet, workaddbuff, workmultbuff] (betaddbuff and betmultbuff will be added later)
+    f.write("{}\n{}\n{}".format(wallet,workaddbuff,workmultbuff))
+    f.close()
+    saveSuccesful = "Game saved."
+    return saveSuccesful
+
+# modifiers for gameplay, subject to change, verbose set to false by default
 playing = True
+verbose = False
 wallet = 10
-workaddbuff, workmultbuff = 0, 1
+workaddbuff, workmultbuff = 10, 2
 
 while playing == True:
     core()
